f68f4d1836f2b96d7a61760e514a7b4311037583
smppatel99999 <94542056+smppatel99999@users.noreply.github.com>
2021-12-13 17:33:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //importing all the modules
 const express = require('express')
 const hbs = require('hbs')
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 const bodyParser = require("body-parser");
 const fileUpload = require('express-fileupload');
 var jsmediatags = require("jsmediatags");
